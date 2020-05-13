f3a2fa6e74de01fe95584a4816522f2644b07b7b
Martae Marshall <martaemarshall@gmail.com>
2020-05-13 16:24:39 -0500

    App modified to show new components

--- helping-hero/src/app/app.module.ts
+++ helping-hero/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //for Ngmodel
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VolunteerComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
