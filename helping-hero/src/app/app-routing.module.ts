0e49cefc3b613a23edb15e53163b99c3c8a83423
Martae Marshall <martaemarshall@gmail.com>
2020-05-19 16:21:53 -0500

    Routing is successful on this application!

--- helping-hero/src/app/app-routing.module.ts
+++ helping-hero/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { VolunteerComponent } from './volunteer/volunteer.component'
+import { RequestComponent } from './request/request.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent},
+  { path: 'volunteer', component: VolunteerComponent },
+  { path: 'request', component: RequestComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'home', component: HomeComponent},
+
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
