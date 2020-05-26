62a1ca97bd3c056b1072af8271647b264a3e6eae
Martae Marshall <martaemarshall@gmail.com>
2020-05-26 10:52:54 -0500

    adjusted object instantiation

--- helping-hero/src/app/volunteer/volunteer.component.ts
+++ helping-hero/src/app/volunteer/volunteer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Volunteer } from '../volunteer'
 
 @Component({
@@ -10,21 +11,22 @@ export class VolunteerComponent {
   services = ['Cleaning', 'Cooking', 'Shopping', 'Walking'];
   
   volunteer = new Volunteer(
-    1,
-    'Johnny',
-    'Doe',
-    'jdoe@email.com',
-    'Kansas City',
-    'MO',
-    '64130',
-    '111-111-1111',
-    '08/01/1995',
+    0,
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    null,
+    null,
     this.services[0]
   );
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  onSubmit() { 
+    console.log('SUBMIT');
+    this.submitted = true; }
 
-  constructor() { }
 }
