b9f11a5cd888497ed7fcb27842e0433b1e92edbe
Martae Marshall <martaemarshall@gmail.com>
2020-05-26 10:49:52 -0500

    functionality to the admin page

--- helping-hero/src/app/admin/admin.component.ts
+++ helping-hero/src/app/admin/admin.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+
+import { VolunteerComponent } from '../volunteer/volunteer.component';
+import { RequestComponent } from '../request/request.component';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  submitted = false;
+
+  onSubmit() { 
+    console.log('submitted');
+    this.submitted = true;
+  }
+
 
   ngOnInit(): void {
   }
